9337fd3cc0246a0739d9fd34f2d5981e9342269b
alone <alone@swufe.edu.cn>
2025-10-25 21:06:16 +0800

    Quartz sync: Oct 25, 2025, 9:06 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "zh-CN",
     baseUrl: "book.jupyter.pro",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
