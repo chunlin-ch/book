6d1a947d21e8d9bdaedd1f03045ab90d0518b56e
alone <alone@swufe.edu.cn>
2025-10-26 19:23:42 +0800

    Quartz sync: Oct 26, 2025, 7:23 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/chunlin-ch/book",
-      CHUNLIN CHEN: "https://chunlin.ch",
+      CHUNLIN: "https://chunlin.ch",
       CC BY 4.0: "https://creativecommons.org/licenses/by/4.0/",
     },
   }),
