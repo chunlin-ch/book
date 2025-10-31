23f958e225e537fb91bea87638a1e8dd3e9b4185
alone <alone@swufe.edu.cn>
2025-10-31 22:41:37 +0800

    Quartz sync: Oct 31, 2025, 10:41 PM

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,13 +1,15 @@
 // @ts-ignore
 import clipboardScript from "./scripts/clipboard.inline"
 import clipboardStyle from "./styles/clipboard.scss"
+import baseStyle from "../styles/base.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { concatenateResources } from "../util/resources"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
   return <div id="quartz-body">{children}</div>
 }
 
 Body.afterDOMLoaded = clipboardScript
-Body.css = clipboardStyle
+Body.css = concatenateResources(baseStyle, clipboardStyle)
 
 export default (() => Body) satisfies QuartzComponentConstructor
